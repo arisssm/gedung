509ed54df49c621a145954b7fe49bb9105bebe64
arisssm <arisssmulianto@gmail.com>
2024-03-07 11:28:58 +0700

    update

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
 const mongoose = require('mongoose');
 const gedungC = require('./controllers/GedungController');
-const Gedung = require('./models/Gedung');
+// const Gedung = require('./models/Gedung');
 
 mongoose.connect('mongodb://localhost:27017/gedung');
 app.use(express.json());
@@ -15,120 +15,119 @@ app.get('/', (req, res) => {
     res.send('hello from simple server YEAH :)')
 });
 
-app.post('/post-gedung', async (req, res) =>{
-    try {
-        const { nama, luas, tingkat, kondisi, parkir } = req.body;
-        console.log(req.body);
-        const createGedung = await Gedung.create({
-            nama,
-            luas,
-            tingkat,
-            kondisi,
-            parkir
-        });
-        res.status(200).json(
-            { 
-                message: 'data tersimpan sebagai berikut:', 
-                gedung: createGedung 
-            }
-        );
-    } catch (error) {
-        res.status(400).json({ message: 'Maaf, data tidak tersimpan', error });
-    }
-});
+// app.post('/post-gedung', async (req, res) =>{
+//     try {
+//         const { nama, luas, tingkat, kondisi, parkir } = req.body;
+//         console.log(req.body);
+//         const createGedung = await Gedung.create({
+//             nama,
+//             luas,
+//             tingkat,
+//             kondisi,
+//             parkir
+//         });
+//         res.status(200).json(
+//             { 
+//                 message: 'data tersimpan sebagai berikut:', 
+//                 gedung: createGedung 
+//             }
+//         );
+//     } catch (error) {
+//         res.status(400).json({ message: 'Maaf, data tidak tersimpan', error });
+//     }
+// });
 
-app.get('/gedung', async (req, res)=>{
-    try{
-        const daftarGedung = await Gedung.find();
-        res.status(200).json(
-            {
-                mesage: 'Berikut data gedung', 
-                gedung: daftarGedung
-            }
-        );
-    } catch(error){
-        res.status(400).json({message: 'Maaf, data belum ada', error});
-    }
-});
+// app.get('/gedung', async (req, res)=>{
+//     try{
+//         const daftarGedung = await Gedung.find();
+//         res.status(200).json(
+//             {
+//                 mesage: 'Berikut data gedung', 
+//                 gedung: daftarGedung
+//             }
+//         );
+//     } catch(error){
+//         res.status(400).json({message: 'Maaf, data belum ada', error});
+//     }
+// });
 
-app.put('/update-gedung', async (req, res)=>{
-    try{
-        const {id, nama, luas, tingkat, kondisi, parkir } = req.body;
-        const gedung = await Gedung.findOne({_id:id});
-        const updateGedung = await Gedung.findOneAndUpdate(
-            {_id:id},
-            {
-                nama: nama,
-                luas: luas,
-                tingkat: tingkat,
-                kondisi: kondisi,
-                parkir: parkir
-            },
-            { new: true } 
-        );
-        if (updateGedung) { res.status(200).json(
-            { 
-                message: 'data berhasil diubah',
-                gedung: gedung,
-                updateGedung: updateGedung
-            });
-        } else {
-            res.status(404).json({ message: 'gedung tidak ada' });
-        }
-    } catch(error){
-        res.status(400).json({message: 'data gagal di ubah', error});
-    }
-});
+// app.put('/update-gedung', async (req, res)=>{
+//     try{
+//         const {id, nama, luas, tingkat, kondisi, parkir } = req.body;
+//         const gedung = await Gedung.findOne({_id:id});
+//         const updateGedung = await Gedung.findOneAndUpdate(
+//             {_id:id},
+//             {
+//                 nama: nama,
+//                 luas: luas,
+//                 tingkat: tingkat,
+//                 kondisi: kondisi,
+//                 parkir: parkir
+//             },
+//             { new: true } 
+//         );
+//         if (updateGedung) { res.status(200).json(
+//             { 
+//                 message: 'data berhasil diubah',
+//                 gedung: gedung,
+//                 updateGedung: updateGedung
+//             });
+//         } else {
+//             res.status(404).json({ message: 'gedung tidak ada' });
+//         }
+//     } catch(error){
+//         res.status(400).json({message: 'data gagal di ubah', error});
+//     }
+// });
 
-app.delete('/gedung/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const delGedung = await Gedung.findOneAndDelete({ _id: id });
-        if (delGedung) {
-            res.status(200).json({
-                message: 'data berhasil dihapus',
-                gedung: delGedung
-            });
-        } else {
-            res.status(404).json(
-                { 
-                    message: 'gedung tidak ditemukan' 
-                }
-            );
-        }
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-});
+// app.delete('/gedung/:id', async (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         const delGedung = await Gedung.findOneAndDelete({ _id: id });
+//         if (delGedung) {
+//             res.status(200).json({
+//                 message: 'data berhasil dihapus',
+//                 gedung: delGedung
+//             });
+//         } else {
+//             res.status(404).json(
+//                 { 
+//                     message: 'gedung tidak ditemukan' 
+//                 }
+//             );
+//         }
+//     } catch (error) {
+//         res.status(400).json({ message: error.message });
+//     }
+// });
 
+// app.get('/detail-gedung/:id', async (req, res) =>{
+//     try{
+//         const {id} = req.params;
+//         const data = await Gedung.findOne({_id:id});
+//         console.log(req.params);
+//         if (!data) {
+//             return res.status(404).json(
+//                 { 
+//                     message: "data dalam pencarian tim SAR" 
+//                 }
+//             );
+//         }
+//         res.status(200).json(
+//             {
+//                 message: 'data berhasil di ambil', data
+//             }
+//         );
+//     } catch(error){
+//         res.status(400).json({ message: error.message});
+//     }
+// });
 
-app.get('/detail-gedung/:id', async (req, res) =>{
-    try{
-        const {id} = req.params;
-        const data = await Gedung.findOne({_id:id});
-        console.log(req.params);
-        if (!data) {
-            return res.status(404).json(
-                { 
-                    message: "data dalam pencarian tim SAR" 
-                }
-            );
-        }
-        res.status(200).json(
-            {
-                message: 'data berhasil di ambil', data
-            }
-        );
-    } catch(error){
-        res.status(400).json({ message: error.message});
-    }
-});
-
-// app.get('/gedung', gedungC.index);
-// app.post('/post-gedung', gedungC.create);
-// app.get('/gedung/:id', gedungC.read);
-// app.put('/update-gedung', gedungC.update);
-// app.delete('/gedung/:id', gedungC.delete);
+app.get('/gedung', gedungC.index);
+app.post('/post-gedung', gedungC.create);
+app.get('/detail-gedung/:id', gedungC.read);
+app.put('/update-gedung', gedungC.update);
+app.delete('/gedung/:id', gedungC.delete);
 
 app.listen(port, () => {
     console.log(`> Server is up and running on port http://localhost:${port}`);
--- controllers/GedungController.js
+++ controllers/GedungController.js
@@ -4,38 +4,43 @@ module.exports = {
     index: async (req, res) => {
         try{
             const daftarGedung = await Gedung.find();
-            res.status(200).json({mesage: 'Berikut data gedung', daftarGedung});
+            res.status(200).json({mesage: 'Data berhasil tampil, berikut data gedung', daftarGedung});
         } catch(error){
-            res.status(400).json({message: 'Maaf, data belum ada', error});
+            res.status(400).json({message: error.message});
         }
     },
     create: async (req, res) => {
         try {
+            const { nama, luas, tingkat, kondisi, parkir } = req.body;
             console.log(req.body);
-            // const { nama, luas, tingkat, kondisi, parkir } = req.body;
-            // if (!nama || !luas || !tingkat || !kondisi || !parkir) {
-            //     return res.status(400).json({ message: 'Payload tidak valid' });
-            // }
-            // const createGedung = await Gedung.create({
-            //     nama,
-            //     luas,
-            //     tingkat,
-            //     kondisi,
-            //     parkir
-            // });
-            // res.status(200).json({ message: 'data tersimpan sebagai berikut:', createGedung });
+            if (!nama || !luas || !tingkat || !kondisi || !parkir) {
+                return res.status(400).json({ message: 'Req body tidak valid' });
+            }
+            const createGedung = await Gedung.create({
+                nama,
+                luas,
+                tingkat,
+                kondisi,
+                parkir
+            });
+            res.status(200).json(
+                { 
+                    message: 'data tersimpan sebagai berikut:', 
+                    gedung: createGedung 
+                }
+            );
         } catch (error) {
-            res.status(400).json({ message: 'Maaf, data tidak tersimpan', error });
+            res.status(400).json({ message: error.message });
         }
     },    
     read: async(req, res) => {
         try{
             const {id} = req.params;
-            const daftar = await Gedung.findOne({_id:id});
+            const data = await Gedung.findOne({_id:id});
             if (!data) {
                 return res.status(404).json({ message: "data dalam pencarian tim SAR" });
             }
-            res.status(200).json({message: 'data berhasil di ambil', daftar});
+            res.status(200).json({message: 'data berhasil di ambil', data});
         } catch(error){
             res.status(400).json({ message: error.message});
         }
@@ -43,23 +48,35 @@ module.exports = {
     update: async(req, res) => {
         try{
             const {id, nama, luas, tingkat, kondisi, parkir } = req.body;
-            await Gedung.updateOne({_id:id},{
-                nama: nama,
-                luas: luas,
-                tingkat: tingkat,
-                kondisi: kondisi,
-                parkir: parkir
-            });
-            res.status(200).json({message: 'data berhasil di ubah, terima kasih'});
+            const gedung = await Gedung.findOne({_id:id});
+            const updateGedung = await Gedung.findOneAndUpdate(
+                {_id:id},
+                {
+                    nama: nama,
+                    luas: luas,
+                    tingkat: tingkat,
+                    kondisi: kondisi,
+                    parkir: parkir
+                },
+                { new: true } 
+            );
+            res.status(200).json({message: 'data berhasil di ubah', gedung: gedung, update: updateGedung});
         } catch(error){
-            res.status(400).json({message: 'data gagal di ubah', error});
+            res.status(400).json({message: error.message});
         }
     },
     delete: async(req, res) => {
         try{
             const {id} = req.params;
-            const daftar = await Gedung.deleteOne({_id:id});
-            res.status(200).json({message: 'berhasil di hapus.. yahh..', daftar});    
+            const delGedung = await Gedung.findOneAndDelete({_id:id});
+            if (delGedung) {
+                res.status(200).json({
+                    message: 'data berhasil dihapus',
+                    gedung: delGedung
+                });
+            } else {
+                res.status(404).json({message: 'gedung tidak ditemukan'});
+            }    
         } catch(error){
             res.status(200).json({message: error.message});    
         }
